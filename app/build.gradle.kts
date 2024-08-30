8ae18d86778c24f3b8d25d95e68046071af2ec98
mertncu <ymuncu@gmail.com>
2024-08-30 21:43:27 +0300

    Refactor Gradle and Libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.cardview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
