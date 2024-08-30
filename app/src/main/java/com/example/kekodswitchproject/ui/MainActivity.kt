7f22b77c04152c75084254f27cf3009942fc8520
mertncu <ymuncu@gmail.com>
2024-08-30 22:50:39 +0300

    Add nav_graph and refactor bottom_nav_menu structure

--- app/src/main/java/com/example/kekodswitchproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/kekodswitchproject/ui/MainActivity.kt
@@ -1,32 +1,65 @@
 package com.example.kekodswitchproject.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
 import com.example.kekodswitchproject.R
 import com.example.kekodswitchproject.databinding.ActivityMainBinding
-import com.example.kekodswitchproject.ui.ego.EgoFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var menu: Menu
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (savedInstanceState == null) {
-            navigateToFragment(EgoFragment())
+        menu = binding.bottomNavigationView.menu
+
+
+        menu.add(Menu.NONE, R.id.egoFragment, Menu.NONE, "Home").setIcon(R.drawable.ic_ego)
+
+
+        binding.bottomNavigationView.visibility = View.GONE
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setOnItemSelectedListener { a ->
+            costomNavigation(a.itemId, navController)
+            true
         }
     }
 
-    private fun navigateToFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
+    fun setBottomNavigationVisibility(visible: Boolean) {
+        binding.bottomNavigationView.visibility = if (visible) View.VISIBLE else View.GONE
     }
+
+
+}
+
+fun costomNavigation(id: Int, navController: NavController) {
+
+    val navOptions = NavOptions.Builder()
+        .setLaunchSingleTop(true)
+        .setPopUpTo(
+            navController.graph.findStartDestination().id,
+            false,
+            saveState = true
+        )
+        .setRestoreState(true)
+        .build()
+
+    navController.navigate(id, null, navOptions)
 }
--- app/src/main/java/com/example/kekodswitchproject/ui/ego/EgoFragment.kt
+++ app/src/main/java/com/example/kekodswitchproject/ui/ego/EgoFragment.kt
@@ -1,220 +1,153 @@
 package com.example.kekodswitchproject.ui.ego
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import com.example.kekodswitchproject.R
 import com.example.kekodswitchproject.databinding.FragmentEgoBinding
-import com.example.kekodswitchproject.ui.anger.AngerFragment
-import com.example.kekodswitchproject.ui.disgust.DisgustFragment
-import com.example.kekodswitchproject.ui.fear.FearFragment
-import com.example.kekodswitchproject.ui.joy.JoyFragment
-import com.example.kekodswitchproject.ui.sadness.SadnessFragment
+import com.example.kekodswitchproject.ui.MainActivity
+
 
 class EgoFragment : Fragment() {
 
-    private var _binding: FragmentEgoBinding? = null
-    private val binding get() = _binding!!
-    private val gifDisplayDuration = 9000L
-    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var binding: FragmentEgoBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentEgoBinding.inflate(inflater, container, false)
+        binding = FragmentEgoBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupSwitchListeners()
-        setupBottomNavigation()
-    }
+        val menu = (activity as MainActivity).menu
 
-    private fun setupSwitchListeners() {
+
+
         binding.switchEgo.setOnCheckedChangeListener { _, isChecked ->
-            handleEgoSwitchChange(isChecked)
-        }
+            (activity as? MainActivity)?.setBottomNavigationVisibility(!isChecked)
+            if (isChecked) {
 
-        val switches = listOf(
-            binding.switch1 to R.drawable.anger_gif,
-            binding.switch2 to R.drawable.disgust_gif,
-            binding.switch3 to R.drawable.joy_gif,
-            binding.switch4 to R.drawable.fear_gif,
-            binding.switch5 to R.drawable.sadness_gif
-        )
+                menu.removeItem(R.id.angerFragment)
+                menu.removeItem(R.id.disgustFragment)
+                menu.removeItem(R.id.fearFragment)
+                menu.removeItem(R.id.joyFragment)
+                menu.removeItem(R.id.sadnessFragment)
 
-        switches.forEach { (switch, gifResId) ->
-            switch.setOnCheckedChangeListener { _, isChecked ->
-                updateSwitchesAndNavigation()
-                if (isChecked) {
-                    showGif(gifResId)
+                with(binding) {
+                    switch1.isEnabled = false
+                    switch2.isEnabled = false
+                    switch3.isEnabled = false
+                    switch4.isEnabled = false
+                    switch5.isEnabled = false
+
+                    switch1.isChecked = false
+                    switch2.isChecked = false
+                    switch3.isChecked = false
+                    switch4.isChecked = false
+                    switch5.isChecked = false
                 }
+            } else {
+
+
+                binding.switch1.isEnabled = true
+                binding.switch2.isEnabled = true
+                binding.switch3.isEnabled = true
+                binding.switch4.isEnabled = true
+                binding.switch5.isEnabled = true
             }
-        }
-    }
 
-    private fun handleEgoSwitchChange(isEgoChecked: Boolean) {
-        toggleSwitches(isEgoChecked)
-        if (isEgoChecked) {
-            disableAllOtherSwitches()
-        }
-        updateSwitchesAndNavigation()
-    }
 
-    private fun toggleSwitches(isEgoChecked: Boolean) {
-        val enabled = !isEgoChecked
-        binding.apply {
-            switch1.isEnabled = enabled
-            switch2.isEnabled = enabled
-            switch3.isEnabled = enabled
-            switch4.isEnabled = enabled
-            switch5.isEnabled = enabled
-            bottomNavigation.visibility = if (enabled) View.VISIBLE else View.GONE
-        }
-    }
-
-    private fun disableAllOtherSwitches() {
-        listOf(
-            binding.switch1,
-            binding.switch2,
-            binding.switch3,
-            binding.switch4,
-            binding.switch5
-        ).forEach { it.isChecked = false }
-    }
-
-    private fun updateSwitchesAndNavigation() {
-        handleSwitchStates()
-        updateBottomNavigation()
-    }
-
-    private fun handleSwitchStates() {
-        val switches = listOf(
-            binding.switch1,
-            binding.switch2,
-            binding.switch3,
-            binding.switch4,
-            binding.switch5
-        )
-
-        val checkedSwitches = switches.filter { it.isChecked }
-
-        if (checkedSwitches.size >= 4) {
-            val remainingSwitch = switches.first { !checkedSwitches.contains(it) }
-            remainingSwitch.isEnabled = false
-            Toast.makeText(
-                requireContext(), "Only 4 switches can be active at a time.", Toast.LENGTH_SHORT
-            ).show()
-        } else {
-            switches.forEach { it.isEnabled = true }
-        }
-    }
-
-    private fun updateBottomNavigation() {
-        val menu: Menu = binding.bottomNavigation.menu
-        menu.clear()
-        addMenuItems(menu)
-        selectFirstItemIfAvailable(menu)
-    }
-
-    private fun addMenuItems(menu: Menu) {
-        if (!binding.switchEgo.isChecked) {
-            menu.add(Menu.NONE, R.id.egoItem, Menu.NONE, "Ego").setIcon(R.drawable.ic_ego)
         }
 
-        val switches = listOf(
-            binding.switch1 to R.id.angerItem,
-            binding.switch2 to R.id.disgustItem,
-            binding.switch3 to R.id.joyItem,
-            binding.switch4 to R.id.fearItem,
-            binding.switch5 to R.id.sadnessItem
-        )
 
-        switches.filter { it.first.isChecked }
-            .take(4)
-            .forEach { (switch, itemId) ->
-                menu.add(Menu.NONE, itemId, Menu.NONE, getMenuTitle(itemId)).setIcon(getMenuIcon(itemId))
-            }
-    }
+        binding.switch1.setOnCheckedChangeListener { _, isChecked ->
 
-    private fun getMenuTitle(itemId: Int): String {
-        return when (itemId) {
-            R.id.angerItem -> "Anger"
-            R.id.disgustItem -> "Disgust"
-            R.id.joyItem -> "Joy"
-            R.id.fearItem -> "Fear"
-            R.id.sadnessItem -> "Sadness"
-            else -> ""
+            clickItems(
+                menu,
+                isChecked,
+                R.id.angerFragment,
+                "Anger",
+                R.drawable.ic_anger,
+            )
+
+
         }
-    }
 
-    private fun getMenuIcon(itemId: Int): Int {
-        return when (itemId) {
-            R.id.angerItem -> R.drawable.ic_anger
-            R.id.disgustItem -> R.drawable.ic_disgust
-            R.id.joyItem -> R.drawable.ic_joy
-            R.id.fearItem -> R.drawable.ic_fear
-            R.id.sadnessItem -> R.drawable.ic_sad
-            else -> 0
+        binding.switch2.setOnCheckedChangeListener { _, isChecked ->
+
+            clickItems(
+                menu, isChecked,
+                R.id.disgustFragment,
+                "Disgust",
+                R.drawable.ic_disgust
+            )
+
+
         }
-    }
 
-    private fun selectFirstItemIfAvailable(menu: Menu) {
-        if (menu.size() > 0) {
-            binding.bottomNavigation.selectedItemId = menu.getItem(0).itemId
+        binding.switch3.setOnCheckedChangeListener { _, isChecked ->
+
+            clickItems(
+                menu,
+                isChecked,
+                R.id.fearFragment,
+                "Fear",
+                R.drawable.ic_fear,
+            )
         }
-    }
 
-    private fun setupBottomNavigation() {
-        binding.bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
-            handleNavigationItemSelected(menuItem.itemId)
+        binding.switch4.setOnCheckedChangeListener { _, isChecked ->
+
+            clickItems(
+                menu,
+                isChecked,
+                R.id.joyFragment,
+                "Joy",
+                R.drawable.ic_joy,
+            )
+
+
         }
-    }
 
-    private fun handleNavigationItemSelected(itemId: Int): Boolean {
-        return when (itemId) {
-            R.id.angerItem -> navigateToFragment(AngerFragment())
-            R.id.disgustItem -> navigateToFragment(DisgustFragment())
-            R.id.joyItem -> navigateToFragment(JoyFragment())
-            R.id.fearItem -> navigateToFragment(FearFragment())
-            R.id.sadnessItem -> navigateToFragment(SadnessFragment())
-            R.id.egoItem -> true
-            else -> false
+        binding.switch5.setOnCheckedChangeListener { _, isChecked ->
+
+            clickItems(
+                menu,
+                isChecked,
+                R.id.sadnessFragment,
+                "Sadness",
+                R.drawable.ic_sad,
+
+                )
+
+
         }
+
     }
 
-    private fun navigateToFragment(fragment: Fragment): Boolean {
-        if (binding.switchEgo.isChecked) return false
-        parentFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null)
-            .commit()
-        return true
-    }
+    fun clickItems(
+        menu: Menu,
+        isChecked: Boolean,
+        fragmentId: Int,
+        title: String,
+        bottomIcon: Int,
 
-    private fun showGif(gifResId: Int) {
-        Glide.with(this)
-            .asGif()
-            .load(gifResId)
-            .into(binding.egoGifImageView)
-        binding.egoGifImageView.visibility = View.VISIBLE
+        ) {
 
-        handler.postDelayed({
-            binding.egoGifImageView.visibility = View.GONE
-        }, gifDisplayDuration)
-    }
+        if (menu.size <= 4 && isChecked && menu.findItem(fragmentId) == null) {
+            menu.add(Menu.NONE, fragmentId, Menu.NONE, title)
+                ?.setIcon(bottomIcon)
+        } else if (!isChecked && menu.findItem(fragmentId) != null) {
+            menu.removeItem(fragmentId)
+        }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
